fe4991d3e3990c34c2f28af1a11cb4455dd75e85
Ryan Muller <cognitionmachine@gmail.com>
2012-02-29 14:20:15 -0500

    set up controllers and views for authors #2

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,6 @@
+class AuthorsController < ApplicationController
+
+  def show
+    @author = Author.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rsrchr::Application.routes.draw do
   resources :pdfhashes
   resources :ref_links, :only => :create
 
+  resources :authors, :only => :show
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
