dd4e25b2c98e2e8881d8d175b0fed26b00c18240
Ryan Muller <cognitionmachine@gmail.com>
2012-02-29 16:22:27 -0500

    added usernames, fixes #4

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+
+  validates :name, :presence => true
+
   has_many :scrobbles
   has_many :ref_links
 end
