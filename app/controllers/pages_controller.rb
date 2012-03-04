e4af9c159c0d2eca9f22d4493bf535949c561ea1
Ryan Muller <cognitionmachine@gmail.com>
2012-03-04 02:57:19 -0500

    handle logged out user for index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
   def index
     @scrobbles = Scrobble.limit(5)
-    @citations = current_user.citations.limit(3)
-    @citations_read = current_user.scrobbles.limit(3).collect { |s| s.citation }.compact
+    if current_user
+      @citations = current_user.citations.limit(3)
+      @citations_read = current_user.scrobbles.limit(3).collect { |s| s.citation }.compact
+    else
+      @citations = [] 
+      @citations_read = []
+    end
   end
 end
